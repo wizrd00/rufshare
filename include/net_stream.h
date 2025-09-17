35547ab3c61b8104c38d6d7a3446f04964d46850
wizrd <swhwap.net@gmail.com>
2025-09-17 23:17:12 +0330

    1.modify tcp socket functions 2.add and modify udp socket functions

--- include/net_stream.h
+++ include/net_stream.h
@@ -3,6 +3,7 @@
 
 #include "types.h"
 #include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <netinet/ip.h>
@@ -10,18 +11,26 @@
 
 #define BACKLOG 1
 
-status_t init_tcp_socket(sockfd_t* fd, ipv4str_t ip, port_t port);
+status_t init_tcp_socket(sockfd_t* fd, ipv4str_t src_ip, port_t dst_port, ipv4str_t dst_ip, port_t dst_port);
 
-status_t set_tcp_socket_rcvbufsize(sockfd_t fd, size_t size);
-
-status_t set_tcp_socket_sndbufsize(sockfd_t fd, size_t size);
-
-status_t set_tcp_socket_timeout(sockfd_t fd, time_t second);
-
-status_t accept_new_connection(sockfd_t* new_fd, sockfd_t fd, struct sockaddr_in* addr);
+status_t accept_new_connection(sockfd_t* new_fd, sockfd_t fd, ipv4str_t conn_ip, port_t* conn_port);
 
 status_t pull_tcp_data(sockfd_t fd, Buffer buf, size_t size);
 
 status_t push_tcp_data(sockfd_t fd, Buffer buf, size_t size);
+
+status_t init_udp_socket(sockfd_t* fd);
+
+status_t pull_udp_data(sockfd_t fd, ipv4str_t conn_ip, port_t* conn_port, Buffer buf, size_t size);
+
+status_t push_udp_data(sockfd_t fd, ipv4str_t dst_ip, port_t dst_port, Buffer buf, size_t size);
+
+status_t set_socket_rcvbufsize(sockfd_t fd, size_t size);
+
+status_t set_socket_sndbufsize(sockfd_t fd, size_t size);
+
+status_t set_socket_timeout(sockfd_t fd, time_t second);
+
+status_t close_socket(sockfd_t fd);
 
 #endif
--- source/net_stream.c
+++ source/net_stream.c
@@ -1,6 +1,6 @@
 #include "net_stream.h"
 
-static status_t convert_ipstring_to_network_byteorder(ipv4str_t ip, uint32_t* dst) {
+static status_t network_byteorder(ipv4str_t ip, uint32_t* dst) {
     status_t stat;
     switch (inet_pton(AF_INET, ip, dst)) {
         case -1 :
@@ -16,48 +16,43 @@ static status_t convert_ipstring_to_network_byteorder(ipv4str_t ip, uint32_t* ds
     return stat;
 }
 
-status_t init_tcp_socket(sockfd_t* fd, ipv4str_t ip, port_t port) {
+static status_t host_ipstring(ipv4str_t ip, struct sockaddr_in* addr) {
     status_t stat = SUCCESS;
-    uint32_t ipnetorder;
-    CHECK_STAT(convert_ipstring_to_network_byteorder(ip, &ipnetorder));
-    struct sockaddr_in addr = {
+    CHECK_PTR(inet_ntop(addr->sin_family, addr, ip, sizeof (struct sockaddr_in)), BADARGS);
+    return stat;
+}
+
+status_t init_tcp_socket(sockfd_t* fd, ipv4str_t src_ip, port_t dst_port, ipv4str_t dst_ip, port_t dst_port) {
+    status_t stat = SUCCESS;
+    uint32_t src_ipnetorder;
+    uint32_t src_ipnetorder;
+    CHECK_STAT(network_byteorder(src_ip, &src_ipnetorder));
+    CHECK_STAT(network_byteorder(dst_ip, &dst_ipnetorder));
+    struct sockaddr_in local_addr = {
         .sin_family = AF_INET,
-        .sin_port = htons(port),
+        .sin_port = htons(src_port),
         .sin_addr = {
-            .s_addr = ipnetorder
+            .s_addr = src_ipnetorder
+        }
+    };
+    struct sockaddr_in remote_addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(dst_port),
+        .sin_addr = {
+            .s_addr = dst_ipnetorder
         }
     };
     CHECK_INT(*fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP), FAILURE);
-    CHECK_INT(connect(*fd, &addr, sizeof (struct sockaddr_in)), FAILURE);
-    CHECK_INT(bind(*fd, &addr, sizeof (struct sockaddr_in)), FAILURE);
+    CHECK_INT(bind(*fd, &local_addr, sizeof (struct sockaddr_in)), FAILURE);
+    CHECK_INT(connect(*fd, &remote_addr, sizeof (struct sockaddr_in)), FAILURE);
     return stat;
 }
 
-status_t set_tcp_socket_rcvbufsize(sockfd_t fd, size_t size) {
-    status_t stat = SUCCESS;
-    CHECK_INT(setsockopt(fd, SOL_SOCKET, SO_RCVBUF, &size, sizeof (size_t)), BADARGS);
-    return stat;
-}
 
-status_t set_tcp_socket_sndbufsize(sockfd_t fd, size_t size) {
-    status_t stat = SUCCESS;
-    CHECK_INT(setsockopt(fd, SOL_SOCKET, SO_SNDBUF, &size, sizeof (size_t)), BADARGS);
-    return stat;
-}
 
-status_t set_tcp_socket_timeout(sockfd_t fd, time_t second) {
+status_t accept_new_connection(sockfd_t* new_fd, sockfd_t fd, ipv4str_t conn_ip, port_t* conn_port) {
     status_t stat = SUCCESS;
-    struct timeval timeout = {
-        .tv_second = second,
-        .tv_usec = 0
-    };
-    CHECK_INT(setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof (struct timeval)), BADARGS);
-    CHECK_INT(setsockopt(fd, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof (struct timeval)), BADARGS);
-    return stat;
-}
-
-status_t accept_new_connection(sockfd_t* new_fd, sockfd_t fd, struct sockaddr_in* addr) {
-    status_t stat = SUCCESS;
+    struct sockaddr_in conn_addr;
     if (listen(fd, BACKLOG) == -1)
         switch (errno) {
             case EBADF :
@@ -72,6 +67,8 @@ status_t accept_new_connection(sockfd_t* new_fd, sockfd_t fd, struct sockaddr_in
 
         }
     CHECK_INT(*new_fd = accept(fd, addr, sizeof (sockaddr_in)), FAILURE);
+    CHECK_STAT(host_ipstring(conn_ip, &conn_addr));
+    *conn_port = ntohs(conn_addr.sin_port);
     return stat;
 }
 
@@ -128,5 +125,125 @@ status_t push_tcp_data(sockfd_t fd, Buffer buf, size_t size) {
         }
     else if (sent_size < size)
         stat = FAILURE;
+    return stat;
+}
+
+status_t init_udp_socket(sockfd_t* fd, ipv4str_t dst_ip, port_t dst_port) {
+    status_t stat = SUCCESS;
+    uint32_t dst_ipnetorder;
+    CHECK_STAT(network_byteorder(dst_ip, &dst_ipnetorder));
+    struct sockaddr_in local_addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(dst_port),
+        .sin_addr = {
+            .s_addr = dst_ipnetorder
+        }
+    };
+    CHECK_INT(*fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP), FAILURE);
+    CHECK_INT(bind(*fd, &local_addr, sizeof (struct sockaddr_in)), FAILURE);
+    return stat;
+}
+
+status_t pull_udp_data(sockfd_t fd, ipv4str_t conn_ip, port_t* conn_port, Buffer buf, size_t size) {
+    status_t stat = SUCCESS;
+    Buffer tmp_buf;
+    struct sockaddr_in conn_addr;
+    ssize_t received_size = recvfrom(fd, tmp_buf, size, 0, &conn_addr, sizeof (struct sockaddr_in));
+    if (received_size == -1)
+        switch (errno) {
+            case EBADF :
+            case EINVAL :
+            case ENOTSOCK :
+                stat = BADARGS;
+                return stat;
+            case EAGAIN :
+            case ECONNREFUSED :
+            case EFAULT :
+            case EINTR :
+            case ENOMEM :
+            default :
+                stat = FAILURE;
+                return stat;
+        }
+    else if (received_size < size)
+        stat = FAILURE;
+    else
+        memcpy(buf, tmp_buf, size);
+    CHECK_STAT(host_ipstring(conn_ip, &conn_addr));
+    *conn_port = ntohs(conn_addr.sin_port);
+    return stat;
+}
+
+status_t push_udp_data(sockfd_t fd, ipv4str_t dst_ip, port_t dst_port, Buffer buf, size_t size) {
+    status_t stat = SUCCESS;
+    uint32_t dst_ipnetorder;
+    CHECK_STAT(network_byteorder(dst_ip, &dst_ipnetorder));
+    struct sockaddr_in remote_addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(dst_port),
+        .sin_addr = {
+            .s_addr = dst_ipnetorder;
+        }
+    };
+    ssize_t sent_size = sendto(fd, buf, size, 0, &remote_addr, sizeof (struct sockaddr_in));
+    if (sent_size == -1)
+        switch (errno) {
+            case EBADF :
+            case EFAULT :
+            case EINVAL :
+            case ENOTSOCK :
+                stat = BADARGS;
+                return stat;
+            case EACCES :
+            case EAGAIN :
+            case ECONNRESET :
+            case EINTR :
+            case ENOBUFS :
+            case ENOMEM :
+            default :
+                stat = FAILURE;
+                return stat;
+        }
+    else if (sent_size < size)
+        stat = FAILURE;
+    return stat;
+}
+
+status_t set_socket_rcvbufsize(sockfd_t fd, size_t size) {
+    status_t stat = SUCCESS;
+    CHECK_INT(setsockopt(fd, SOL_SOCKET, SO_RCVBUF, &size, sizeof (size_t)), BADARGS);
+    return stat;
+}
+
+status_t set_socket_sndbufsize(sockfd_t fd, size_t size) {
+    status_t stat = SUCCESS;
+    CHECK_INT(setsockopt(fd, SOL_SOCKET, SO_SNDBUF, &size, sizeof (size_t)), BADARGS);
+    return stat;
+}
+
+status_t set_socket_timeout(sockfd_t fd, time_t second) {
+    status_t stat = SUCCESS;
+    struct timeval timeout = {
+        .tv_second = second,
+        .tv_usec = 0
+    };
+    CHECK_INT(setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof (struct timeval)), BADARGS);
+    CHECK_INT(setsockopt(fd, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof (struct timeval)), BADARGS);
+    return stat;
+}
+
+status_t close_socket(sockfd_t fd) {
+    status_t stat = SUCCESS;
+    if (close(fd) == -1)
+        switch (errno) {
+            case EBADF :
+                stat = BADARGS;
+                return stat;
+            case EINTR :
+            case EIO :
+            default :
+                stat = FAILURE;
+                return stat;
+        }
     return stat;
 }
