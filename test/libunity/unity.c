548185cd03e3d73e1f4db33ac7e5b53c7656e425
wizard <swhwap.net@gmail.com>
2025-10-03 13:42:52 +0330

    Update unity.c

--- test/libunity/unity.c
+++ test/libunity/unity.c
@@ -1,10 +1,3 @@
-/* =========================================================================
-    Unity - A Test Framework for C
-    ThrowTheSwitch.org
-    Copyright (c) 2007-25 Mike Karlesky, Mark VanderVoord, & Greg Williams
-    SPDX-License-Identifier: MIT
-========================================================================= */
-
 #include "unity.h"
 
 #ifndef UNITY_PROGMEM
