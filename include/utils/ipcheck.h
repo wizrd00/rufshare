bc000cf7c32dfe502d2f6a3f2ffb7d270f2a4741
wizrd <swhwap.net@gmail.com>
2025-10-08 13:00:25 +0330

    add test/test_cntl directory

---
+++ include/utils/ipcheck.h
@@ -0,0 +1,15 @@
+#ifndef IPCHECK_H
+#define IPCHECK_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+
+static inline bool check_ipv4_format(const char *ip) {
+    uint32_t ipnetorder;
+    if (inet_pton(AF_INET, ip, &ipnetorder) != 1)
+        return false;
+    return true;
+}
+
+#endif
