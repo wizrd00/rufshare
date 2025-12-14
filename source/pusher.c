3cf0fa34bf1b3ca4a792679897791999eede402a
wizrd <wizrd.bin@proton.me>
2025-12-14 16:08:53 +0330

    add LOGD to pusher module

--- source/pusher.c
+++ source/pusher.c
@@ -6,6 +6,7 @@ static status_t push_handshake(void)
 	HeaderArgs header;
 	LOGT("in function push_handshake()");
 	conf->chcount = calc_chunk_count(conf->filec.size, conf->chsize, &conf->pchsize);
+	LOGD("chunk count = %lu", conf->chcount);
 	header.send.packet = pack_RUFShare_SendPacket(conf->chsize, conf->chcount, conf->pchsize, 0);
 	sstrncpy(header.send.info.filename, conf->addrs.filename, MAXFILENAMESIZE);
 	sstrncpy(header.send.info.name, conf->addrs.name, MAXNAMESIZE);
@@ -13,8 +14,11 @@ static status_t push_handshake(void)
 	sstrncpy(header.send.info.remote_ip, conf->addrs.remote_ip, MAXIPV4SIZE);
 	header.send.info.local_port = conf->addrs.local_port;
 	header.send.info.remote_port = conf->addrs.remote_port;
+	LOGD("SEND packet prepared and it is ready to push");
 	CHECK_STAT(push_SEND_header(conf->cntl_sock, &header, conf->hst_send), "push_SEND_header() failed");
+	LOGD("SEND packet pushed");
 	CHECK_STAT(pull_RECV_header(conf->cntl_sock, &header, conf->hst_recv), "pull_RECV_header() failed");
+	LOGD("RECV packet pulled");
 	if (header.recv.packet.ack == 0)
 		_stat = ZEROACK;
 	conf->seq = 1;
@@ -32,13 +36,18 @@ static status_t push_transfer(void)
 	int trycount = conf->tf_trycount;
 	LOGT("in function push_transfer()");
 	while (conf->seq <= conf->chcount) {
+		LOGD("preparing chunk with seq = %lu", conf->seq);
 		chcon.start_pos = (conf->seq - 1) * conf->chsize;
 		chcon.chunk_size = (conf->seq == conf->chcount) ? conf->pchsize : conf->chsize;
 		crc = calc_chunk_crc32(&conf->filec, &chcon);
+		LOGD("CRC32 of chunk calculated with value = %zd", crc);
 		flow_header.flow.packet = pack_RUFShare_FlowPacket(chcon.chunk_size, conf->seq, crc);
+		LOGD("FLOW packet prepared and it is ready to push");
 		while (trycount != 0) {
 			CHECK_STAT(push_FLOW_header(conf->cntl_sock, &flow_header, conf->tft_flow), "push_FLOW_header() failed");
+			LOGD("FLOW packet pushed");
 			CHECK_STAT(pull_RECV_header(conf->cntl_sock, &recv_header, conf->tft_recv), "pull_RECV_header() failed");
+			LOGD("RECV packet pulled");
 			if (recv_header.recv.packet.ack == 1)
 				break;
 			else
@@ -48,7 +57,9 @@ static status_t push_transfer(void)
 		trycount = conf->tf_trycount;
 		while (trycount != 0) {
 			CHECK_STAT(push_chunk_data(conf->data_sock, &conf->filec, &chcon, conf->tft_data), "push_chunk_data() failed");
+			LOGD("chunk of the data pushed");
 			CHECK_STAT(pull_RECV_header(conf->cntl_sock, &recv_header, conf->tft_recv), "pull_RECV_header() failed");
+			LOGD("RECV packet pulled");
 			if (recv_header.recv.packet.ack == 1)
 				break;
 			else
@@ -69,6 +80,7 @@ static status_t push_verification(void)
 	HeaderArgs header;
 	LOGT("in function push_verification()");
 	RUFShareCRC16 crc = calc_file_crc16(&conf->filec);
+	LOGD("CRC16 of file calculated with value = %lu", crc);
 	header.send.packet = pack_RUFShare_SendPacket(conf->chsize, conf->chcount, conf->pchsize, crc);
 	sstrncpy(header.send.info.filename, conf->addrs.filename, MAXFILENAMESIZE);
 	sstrncpy(header.send.info.name, conf->addrs.name, MAXNAMESIZE);
@@ -76,8 +88,11 @@ static status_t push_verification(void)
 	sstrncpy(header.send.info.remote_ip, conf->addrs.remote_ip, MAXIPV4SIZE);
 	header.send.info.local_port = conf->addrs.local_port;
 	header.send.info.remote_port = conf->addrs.remote_port;
+	LOGD("SEND packet prepared and it is ready to push");
 	CHECK_STAT(push_SEND_header(conf->cntl_sock, &header, conf->vft_send), "push_SEND_header() failed");
+	LOGD("SEND packet pushed");
 	CHECK_STAT(pull_RECV_header(conf->cntl_sock, &header, conf->vft_recv), "pull_RECV_header() failed");
+	LOGD("RECV packet pulled");
 	if (header.recv.packet.ack == 0)
 		_stat = ZEROACK;
 	LOGT("return from push_verification()");
